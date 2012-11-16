99459c47fdc746f40076b3807c4483e11168ffe0
Borja Salguero <borja.salguero@gmail.com>
2012-11-16 22:41:29 +0100

    Wifi polish

--- apps/communications/ftu/js/navigation.js
+++ apps/communications/ftu/js/navigation.js
@@ -45,7 +45,6 @@ var Navigation = {
     var currentStep = steps[this.currentStep];
     if (window.location.hash != currentStep.hash) {
       UIManager.navBar.classList.remove('back-only');
-      UIManager.navBar.classList.remove('secondary-menu');
       window.history.back();
     } else {
       this.currentStep--;
@@ -64,24 +63,28 @@ var Navigation = {
   handleEvent: function n_handleEvent(event) {
     switch (window.location.hash) {
       case '#languages':
-        UIManager.progressBar.className = "step-state step-1";
+        UIManager.progressBar.className = 'step-state step-1';
         UIManager.mainTitle.innerHTML = _('language');
         UIManager.activationScreen.classList.add('no-options');
         break;
       case '#wifi':
-        UIManager.progressBar.className = "step-state step-2";
+        UIManager.progressBar.className = 'step-state step-2';
         UIManager.mainTitle.innerHTML = _('wifi');
         UIManager.activationScreen.classList.remove('no-options');
-        UIManager.navBar.classList.remove('secondary-menu');
+        if (UIManager.navBar.classList.contains('secondary-menu')) {
+          UIManager.navBar.classList.remove('secondary-menu');
+          return;
+        }
+        // Avoid refresh when connecting
         WifiManager.scan(UIManager.renderNetworks);
         break;
       case '#date_and_time':
-        UIManager.progressBar.className = "step-state step-3";
+        UIManager.progressBar.className = 'step-state step-3';
         UIManager.mainTitle.innerHTML = _('dateAndTime');
         UIManager.activationScreen.classList.add('no-options');
         break;
       case '#import_contacts':
-        UIManager.progressBar.className = "step-state step-4";
+        UIManager.progressBar.className = 'step-state step-4';
         UIManager.mainTitle.innerHTML = _('importContacts');
         var fbOption = document.getElementById('fb_import');
         if (WifiManager.isConnected) {
@@ -91,15 +94,15 @@ var Navigation = {
         }
         break;
       case '#welcome_firefox':
-        UIManager.progressBar.className = "step-state step-5";
+        UIManager.progressBar.className = 'step-state step-5';
         UIManager.mainTitle.innerHTML = _('firefoxPrivacyChoices');
         break;
       case '#about-your-rights':
-        UIManager.progressBar.className = "step-state step-5";
+        UIManager.progressBar.className = 'step-state step-5';
         UIManager.navBar.classList.add('back-only');
         break;
       case '#about-your-privacy':
-        UIManager.progressBar.className = "step-state step-5";
+        UIManager.progressBar.className = 'step-state step-5';
         UIManager.navBar.classList.add('back-only');
         break;
       default:
